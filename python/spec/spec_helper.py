a7e7a75e58ee7a493e5b0105fb43d15177a1d089
AndyDeany <oneandydean@hotmail.com>
2016-12-12 18:23:23 +0000

    Update spec_helper.py to load all functions in python/func too

--- python/spec/spec_helper.py
+++ python/spec/spec_helper.py
@@ -13,6 +13,9 @@ execfile("helper/error_logging.py")
 execfile("helper/asset_loading.py")
 execfile("helper/initialise_screen.py")
 
+for python_file in os.listdir("func"):
+    execfile("".join(("func\\", python_file)))
+
 execfile("helper/abstract_classes.py")
 for python_file in os.listdir("lib"):
     execfile("".join(("lib\\", python_file)))
