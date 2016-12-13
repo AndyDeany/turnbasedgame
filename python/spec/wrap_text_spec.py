66535516334070ba425c2c153e025fceff03b588
AndyDeany <oneandydean@hotmail.com>
2016-12-13 17:43:11 +0000

    Change first wrap_text() unit test to fully test the functionality of wrap text, most notably to catch the major bug that was present in wrap_text() before

--- python/spec/wrap_text_spec.py
+++ python/spec/wrap_text_spec.py
@@ -13,11 +13,31 @@ with description("wrap_text()"):
                 "elementum. Etiam ultrices molestie sem vitae accumsan. "
                 "Maecenas dictum gravida nulla non blandit.")
         lines = wrap_text(text, font, 200)
-
-        for line in lines:
-            expect(font.size(line)[0]).to(be_below_or_equal(200))
-
-        expect(" ".join(lines)).to(equal(text))
+        expect(lines).to(equal(["Lorem ipsum dolor sit",
+                                "amet, consectetur",
+                                "adipiscing elit. Aliquam",
+                                "faucibus magna arcu,",
+                                "nec finibus lectus",
+                                "gravida ultrices. Nam",
+                                "non nibh tellus.",
+                                "Pellentesque molestie",
+                                "sagittis mi eget",
+                                "interdum. Vivamus",
+                                "semper metus",
+                                "vestibulum metus",
+                                "interdum porttitor.",
+                                "Phasellus suscipit enim",
+                                "at sollicitudin luctus.",
+                                "Nam ultrices quis leo a",
+                                "faucibus. Mauris porta",
+                                "nibh ultrices quam",
+                                "bibendum, sed cursus",
+                                "velit elementum. Etiam",
+                                "ultrices molestie sem",
+                                "vitae accumsan.",
+                                "Maecenas dictum",
+                                "gravida nulla non",
+                                "blandit."]))
 
     with it("should create a forced newline wherever \\n is present in the text"):
         font = load_font("chewy", 20)
@@ -28,6 +48,7 @@ with description("wrap_text()"):
     with it("should raise an error if a word is too long to wrap"):
         font = load_font("chewy", 20)
         text = ("Supercalifragilisticexpialidocious.")
+
         def give_invalid_text():
             wrap_text(text, font, 200)
         expect(give_invalid_text).to(raise_error(ValueError))
