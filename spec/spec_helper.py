fc1c731c929db703786f3e3734ff3880eaff901e
AndyDeany <oneandydean@hotmail.com>
2016-12-22 18:17:41 +0000

    Change TestGame.log() function to simply raise errors. This suffices for testing purposes

--- spec/spec_helper.py
+++ spec/spec_helper.py
@@ -11,25 +11,8 @@ class TestGame(Game):
         super(TestGame, self).__init__(resolution)
 
     def log(self, *error_message):
-        """Altered log function which doesn't show error popups."""
-        try:
-            error_message = "".join(map(str, error_message))
-            with open(self.path_to("log.txt"), "a") as error_log:
-                error_log.write("".join((
-                    str(datetime.datetime.utcnow())[0:19], " - ",
-                    error_message, ": ",
-                    str(self.error), " (", self.error.__class__.__name__, ")\n"
-                    )))
-            self.error = "Details unknown"  # Resetting to default value
-        except:    # Likely only when file_directory has not yet been defined
-            raise
-        #! Add some code here to show a message in game instead of
-        # force quitting the game unless the error is sufficiently bad.
-        # fatal_error (below) should depend on this code,
-        # or it can be passed in to log() as a argument.
-        fatal_error = True
-        if fatal_error:
-            raise
+        """Altered log function which just raises errors."""
+        raise
 
 
 game = TestGame((1280, 720))
