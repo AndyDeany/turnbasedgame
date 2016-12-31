1737ed4fc6f05ccf36b81fa3ed37137f0fafa93b
AndyDeany <oneandydean@hotmail.com>
2016-12-31 01:46:41 +0000

    Change displaying of item info to be significantly more CPU efficient, but slightly less RAM efficient. Refactor creation of info surface

--- lib/item.py
+++ lib/item.py
@@ -9,9 +9,7 @@ class Item(OneImage):
             self.load_class_assets()
 
         self.name = name
-        self.description = self.game.helper.wrap_text(description,
-                                                      self.description_font,
-                                                      self.info_text_width)
+        self.info = self.create_info_surface(description)
 
     def load_class_assets(self):
         self.game.helper.load_class_assets(self, {
@@ -19,47 +17,62 @@ class Item(OneImage):
             "description_font": self.game.load_font("chewy", 20)
         })
 
-    def display(self, coordinates):
-        self.load_image()
-        self.game.screen.blit(self.image, coordinates)
-
-    # Class attributes used in display_info()
+    # Class attributes used in create_description_surface()
     info_text_padding = 5
     info_text_width = 200
+    info_border_width = 1
     info_border_colour = (102, 51, 0)
     info_background_colour = (182, 155, 76)
     info_text_colour = (0, 0, 0)
 
-    def display_info(self, coordinates):
-        """Displays the items name and description at the given coordinates"""
-        info_width = 2*self.info_text_padding + self.info_text_width
-        name_height = 2*self.info_text_padding + self.name_font.size(self.name)[1]
-        description_line_height = self.description_font.size(self.description[-1])[1]
-        description_height = (2*self.info_text_padding
-                              + len(self.description) * description_line_height)
+    def create_info_surface(self, description):
+        """Creates a surface showing the name and description of the item."""
+        def with_padding(dimension):
+            """Adds text padding to a width or height measurement."""
+            return 2*self.info_text_padding + dimension
 
-        name_rect = self.game.pygame.Rect(coordinates, (info_width, name_height))
-        description_rect = self.game.pygame.Rect(
-            (coordinates[0], coordinates[1] + name_height),
-            (info_width, description_height)
-        )
+        def draw_border_rect(Rect):
+            self.game.pygame.draw.rect(surface, self.info_border_colour,
+                                       Rect, self.info_border_width)
 
-        self.game.pygame.draw.rect(self.game.screen, self.info_background_colour, name_rect)
-        self.game.pygame.draw.rect(self.game.screen, self.info_border_colour, name_rect, 1)
-        self.game.pygame.draw.rect(self.game.screen, self.info_background_colour, description_rect)
-        self.game.pygame.draw.rect(self.game.screen, self.info_border_colour, description_rect, 1)
+        description = self.game.helper.wrap_text(description,
+                                                 self.description_font,
+                                                 self.info_text_width)
 
-        self.game.screen.blit(
-            self.name_font.render(self.name, True, self.info_text_colour),
-            (coordinates[0] + self.info_text_padding, coordinates[1] + self.info_text_padding)
-        )
+        surface_width = with_padding(self.info_text_width)
+        name_height = with_padding(self.name_font.size(self.name)[1])
+        description_line_height = self.description_font.size("")[1]
+        description_height = with_padding(len(description) * description_line_height)
+        surface_height = name_height + description_height
 
-        for index in range(len(self.description)):
-            self.game.screen.blit(
-                self.description_font.render(
-                    self.description[index], True, self.info_text_colour
-                    ),
-                (coordinates[0] + self.info_text_padding,
-                 coordinates[1] + name_height
-                 + self.info_text_padding + index * description_line_height)
+        surface = self.game.pygame.Surface((surface_width, surface_height))
+
+        name_rect = self.game.pygame.Rect((0, 0), (surface_width, name_height))
+        description_rect = self.game.pygame.Rect((0, name_height),
+                                                 (surface_width, description_height))
+        surface.fill(self.info_background_colour)
+        draw_border_rect(name_rect)
+        draw_border_rect(description_rect)
+
+        surface.blit(self.name_font.render(self.name, True, self.info_text_colour),
+                     (self.info_text_padding, self.info_text_padding))
+
+        for index in range(len(description)):
+            surface.blit(
+                self.description_font.render(description[index], True,
+                                             self.info_text_colour),
+                (self.info_text_padding,
+                 name_height
+                 + self.info_text_padding
+                 + index * description_line_height)
             )
+
+        return surface
+
+    def display(self, coordinates):
+        self.load_image()
+        self.game.screen.blit(self.image, coordinates)
+
+    def display_info(self, coordinates):
+        """Displays the item's name and description at the given coordinates."""
+        self.game.screen.blit(self.info, coordinates)
