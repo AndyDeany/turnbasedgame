5f6902f0502c61d3a49ef9eead863cbd5e4b3690
AndyDeany <oneandydean@hotmail.com>
2017-01-18 21:13:45 +0000

    Update item spec to update changes to Item class (whoops shoulda done this first I guess :pensive:)

--- spec/item_spec.py
+++ spec/item_spec.py
@@ -12,5 +12,5 @@ with description("Item"):
             "heart"
             )
         expect(heart.name).to(equal("Heart"))
-        expect(heart.description).to(equal(["A lovely heart."]))
+        expect(heart.info).to(be_a(game.pygame.Surface))
         expect(heart.image_name).to(equal("items/heart"))
