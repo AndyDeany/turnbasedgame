d43d5450df023348057c3fb62b3f1963715d7eef
AndyDeany <oneandydean@hotmail.com>
2017-01-18 21:20:11 +0000

    Add new unit tests for Helper.wrap_text for robustness

--- spec/helper/wrap_text_spec.py
+++ spec/helper/wrap_text_spec.py
@@ -1,8 +1,11 @@
 execfile("spec/spec_helper.py")
 
 with description("wrap_text()"):
-    with it("should wrap text correctly"):
+    with before.all:
+        global font
         font = game.load_font("chewy", 20)
+
+    with it("should wrap text correctly"):
         text = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 "Aliquam faucibus magna arcu, nec finibus lectus gravida "
                 "ultrices. Nam non nibh tellus. Pellentesque molestie "
@@ -40,15 +43,21 @@ with description("wrap_text()"):
                                 "blandit."]))
 
     with it("should create a forced newline wherever \\n is present in the text"):
-        font = game.load_font("chewy", 20)
         text = "Lorem\nipsum\ndolor\nsit\namet."
         lines = game.helper.wrap_text(text, font, 200)
         expect(lines).to(equal(["Lorem", "ipsum", "dolor", "sit", "amet."]))
 
-    with it("should raise an error if a word is too long to wrap"):
-        font = game.load_font("chewy", 20)
+    with it("should raise an error if the first word is too long to wrap"):
         text = "Supercalifragilisticexpialidocious."
 
-        def give_invalid_text():
+        def use_invalid_text():
             game.helper.wrap_text(text, font, 200)
-        expect(give_invalid_text).to(raise_error(ValueError))
+        expect(use_invalid_text).to(raise_error(ValueError))
+
+    with it("should raise an error if a word in the middle is too long to wrap"):
+        text = "I am feeling supercalifragilisticexpialidocious today!"
+
+        def use_invalid_text():
+            game.helper.wrap_text(text, font, 200)
+        expect(use_invalid_text).to(raise_error(ValueError))
+
