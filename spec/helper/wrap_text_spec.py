bf03a7fc52da012999eff47a4290cb6bcc094289
AndyDeany <oneandydean@hotmail.com>
2017-01-26 19:15:22 +0000

    Change first wrap_text() unit test so it works on all operating systems

--- spec/helper/wrap_text_spec.py
+++ spec/helper/wrap_text_spec.py
@@ -5,7 +5,7 @@ with description("wrap_text()"):
         global font
         font = game.load_font("chewy", 20)
 
-    with it("should wrap text correctly"):
+    with it("should wrap valid text correctly"):
         text = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 "Aliquam faucibus magna arcu, nec finibus lectus gravida "
                 "ultrices. Nam non nibh tellus. Pellentesque molestie "
@@ -16,31 +16,12 @@ with description("wrap_text()"):
                 "elementum. Etiam ultrices molestie sem vitae accumsan. "
                 "Maecenas dictum gravida nulla non blandit.")
         lines = game.helper.wrap_text(text, font, 200)
-        expect(lines).to(equal(["Lorem ipsum dolor sit",
-                                "amet, consectetur",
-                                "adipiscing elit. Aliquam",
-                                "faucibus magna arcu,",
-                                "nec finibus lectus",
-                                "gravida ultrices. Nam",
-                                "non nibh tellus.",
-                                "Pellentesque molestie",
-                                "sagittis mi eget",
-                                "interdum. Vivamus",
-                                "semper metus",
-                                "vestibulum metus",
-                                "interdum porttitor.",
-                                "Phasellus suscipit enim",
-                                "at sollicitudin luctus.",
-                                "Nam ultrices quis leo a",
-                                "faucibus. Mauris porta",
-                                "nibh ultrices quam",
-                                "bibendum, sed cursus",
-                                "velit elementum. Etiam",
-                                "ultrices molestie sem",
-                                "vitae accumsan.",
-                                "Maecenas dictum",
-                                "gravida nulla non",
-                                "blandit."]))
+        for index in range(len(lines)):
+            line = lines[index]
+            expect(font.size(line)[0]).to(be_below_or_equal(200))   # Line not too long
+            if not index + 1 == len(lines):
+                next_word = lines[index+1].split()[0]
+                expect(font.size(" ".join((line, next_word)))).to(be_above(200))    # Line not too short
 
     with it("should create a forced newline wherever \\n is present in the text"):
         text = "Lorem\nipsum\ndolor\nsit\namet."
@@ -60,4 +41,3 @@ with description("wrap_text()"):
         def use_invalid_text():
             game.helper.wrap_text(text, font, 200)
         expect(use_invalid_text).to(raise_error(ValueError))
-
