3dc47a39127782aa165a15d1ba268d62f157e8f3
AndyDeany <oneandydean@hotmail.com>
2017-02-08 16:10:27 +0000

    Tidy up Item class spec

--- spec/item_spec.py
+++ spec/item_spec.py
@@ -4,12 +4,7 @@ from lib.item import Item
 
 with description("Item"):
     with it("should initialise"):
-        heart = Item(
-            game,
-            "Heart",
-            "A lovely heart.",
-            "heart"
-            )
+        heart = Item(game, "Heart", "A lovely heart.", "heart")
         expect(heart.name).to(equal("Heart"))
         expect(heart.info).to(be_a(game.pygame.Surface))
         expect(heart.image_name).to(equal("items/heart"))
